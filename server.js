a8e40504fb76791a9cc6021c5f7e48a3b6e994b7
theotheories <57911269+theotheories@users.noreply.github.com>
2021-04-21 19:51:37 +0100

    health check done, nodemon running. now setting up user of mongodb

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ import express from  "express";
 import mongoose from "mongoose";
 
 // App config
+const app = express();
+const port = process.env.PORT || 8001;
 
 // middlewares
 
 // db config
 
 // api endpoints
+app.get("/", (request, response) => response.status(200).send("hello"));
 
 // listener
+app.listen(port, () => console.log(`listening on localhost: ${port}`));
